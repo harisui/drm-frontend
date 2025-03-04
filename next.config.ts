ec3fa8b5a5690df80c7fd8072a75653906b431d6
harisui <root@srv725502.hstgr.cloud>
2025-03-04 21:24:02 +0000

    ts disalbed 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        minimumCacheTTL: 60,
+        domains: [''],
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                pathname: '**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
