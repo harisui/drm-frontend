14feec29551a0919068d7b039e4b71cbeef20971
Haris <haris1192317043@gmai.com>
2025-06-03 13:47:43 +0500

    few fixes.

--- src/app/fullreport/_components/printable-report.tsx
+++ src/app/fullreport/_components/printable-report.tsx
@@ -172,7 +172,7 @@ const PrintableReport = ({ params, report }: PrintableReportProps) => {
 
         <div className="p-2 mb-1 flex justify-center items-center  rounded-md">
           {hasData ? (
-            <div style={{ width: '700px', height: '400px' }}>
+            <div style={{ width: '500px', height: '300px' }}>
               <ChartContainer config={chartConfig}>
                 <AreaChart
                   data={chartData}
