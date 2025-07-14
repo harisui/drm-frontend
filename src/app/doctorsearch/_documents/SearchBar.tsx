0f1839bfe22c83b80852d93a7298966d4576d8de
Touseef HussainE <133860155+touseefhus@users.noreply.github.com>
2025-07-14 16:45:39 +0500

    push reduce search indent

--- src/app/doctorsearch/_documents/SearchBar.tsx
+++ src/app/doctorsearch/_documents/SearchBar.tsx
@@ -20,9 +20,8 @@ const SearchBar: React.FC<SearchBarProps> = ({
   <div className="relative mt-6 group transition-all duration-500 w-full">
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className={`absolute left-4 top-6 sm:top-8 h-6 w-6 -translate-y-1/2 text-gray-400 transition-all duration-300 ${
-        isSearchFocused ? "left-6 h-7 w-7" : ""
-      }`}
+      className={`absolute left-2 sm:left-4 top-6 sm:top-8 h-6 sm:w-6 w-5 -translate-y-1/2 text-gray-400 transition-all duration-300 ${isSearchFocused ? "left-1 sm:left-6 h-7 w-7" : ""
+        }`}
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
@@ -38,12 +37,11 @@ const SearchBar: React.FC<SearchBarProps> = ({
       onBlur={onBlur}
       type="text"
       placeholder="Search doctor by name or department"
-      className={`w-full rounded-lg bg-white py-3 pl-12 pr-4 text-base shadow-lg outline-none ring-1 ring-gray-100 lg:py-4 lg:text-lg
+      className={`w-full rounded-lg bg-white py-3 pl-7 sm:pl-10 pr-4 text-base shadow-lg outline-none ring-1 ring-gray-100 lg:py-4 lg:text-lg
         transition-all duration-500 ease-in-out
-        ${
-          isSearchFocused
-            ? "scale-105 shadow-xl ring-2 ring-blue-400 pl-14"
-            : ""
+        ${isSearchFocused
+          ? "scale-105 shadow-xl ring-2 ring-blue-400 pl-10 sm:pl-14"
+          : ""
         }`}
     />
     <div className="mt-2 text-xs text-gray-500">
